b9929ae4069ae6c2addeae69f87bcdae5cf1a783
Dimitri Kobassian <d.kobassian@gmail.com>
2024-03-18 13:46:27 +0100

    Implementation of the login functionality, visuals WIP

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,19 +1,30 @@
-fetch('http://localhost:5678/api/login', {
-    method: 'POST',
+// Add event listener to the form and prevent the default action
+document.addEventListener("submit", (connect) => {
+    connect.preventDefault();
+let form = {
+    email: document.getElementById("email"),
+    password: document.getElementById("password"),
+};
+
+// Fetch the API and send the stringified data
+fetch('http://localhost:5678/api/users/login', {
+    method: "POST",
     headers: {
-        'Content-Type': 'application/json'
+    Accept: "application/json",
+    "Content-Type": "application/json",
     },
     body: JSON.stringify({
-        username: 'your_username',
-        password: 'your_password'
-    })
-})
-    .then(response => response.json())
-    .then(data => {
-        // Handle the response data here
-        console.log(data);
-    })
-    .catch(error => {
-        // Handle any errors that occur during the request
-        console.error(error);
+    email: form.email.value,
+    password: form.password.value,
+    }),
+}).then((response) => {
+    if (response.status !== 200) {
+    alert("Email ou mot de passe erronés"); //Display an alert if the status is not 200
+    } else {
+    response.json().then((data) => {
+        sessionStorage.setItem("token", data.token); //store the token in the session storage
+        window.location.replace("index.html"); //redirect to the index page
     });
+    }
+});
+});
